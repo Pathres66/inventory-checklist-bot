13313acb581b2364b4244e81f84255f4fca3884d
Pathres <feky66@gmail.com>
2026-06-30 17:47:14 +0200

    Fix config for Railway env variables

--- config.py
+++ config.py
@@ -6,8 +6,11 @@ load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+print("DISCORD TOKEN FOUND:", bool(DISCORD_TOKEN))
+print("DATABASE URL FOUND:", bool(DATABASE_URL))
+
 if not DISCORD_TOKEN:
-    raise RuntimeError("Missing DISCORD_TOKEN in .env")
+    raise RuntimeError("Missing DISCORD_TOKEN")
 
 if not DATABASE_URL:
-    raise RuntimeError("Missing DATABASE_URL in .env")
+    raise RuntimeError("Missing DATABASE_URL")
